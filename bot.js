28de971aa58a6e0d63b736af476898758b0b2cbe
Amol Rama <43121460+AmoleR@users.noreply.github.com>
2020-09-07 17:01:37 -0700

    Making commands similar to README.md

--- bot.js
+++ bot.js
@@ -13,8 +13,8 @@ const { exec } = require('child_process');
 const { getContestPage, getShortContestPage } = require('./helpers/contestInfo.js');
 const { createReactions, createMessages } = require('./helpers/createCollectors.js');
 
-const prefix = process.env.CUSTOM_PREFIX || 'gimme ';
-const mod_prefix = process.env.MOD_PREFIX || 'do ';
+const prefix = process.env.CUSTOM_PREFIX || 'gimme';
+const mod_prefix = process.env.MOD_PREFIX || 'do';
 
 const noAsy = str => {
   while (str.includes('[asy]') && str.includes('[/asy]')) {
@@ -555,7 +555,7 @@ client.on('message', async message => {
   if (!message.content.startsWith(prefix) && !message.content.includes('<@!' + client.user.id + '>')) {
     return;
   }
-  message.content = message.content.replace(new RegExp(prefix, 'g'), '').replace(new RegExp('<@!' + client.user.id + '>', 'g'), '');
+  message.content = message.content.replace(new RegExp(prefix, 'g'), '').replace(new RegExp('<@!' + client.user.id + '>', 'g'), '').trim();
   if (message.content.toLowerCase().includes('ping')) {
      const msg = await message.reply(`Pong`);
      msg.edit(`<@` + message.author.id + `>, Pong: ${msg.createdTimestamp - message.createdTimestamp} ms.`);
@@ -570,24 +570,50 @@ client.on('message', async message => {
     const helpEmbed  = new Discord.MessageEmbed()
     	.setColor('#0099ff')
     	.setTitle('Contest Bot')
-    	.setURL('https://heroku.com/')
-    	.setAuthor('Amol Rama', 'https://i.redd.it/7i52f6n4iely.jpg')
-    	.setDescription('This is a bot that takes problems from AoPS (by request), puts them in a database, and anyone can find it with the appropriate command.')
+    	.setURL('https://cryptic-hamlet-37911.herokuapp.com/')
+    	.setAuthor('Discord Contest Bot Team')
+    	.setDescription('Contest Bot is a Discord Bot dedicated to making math competition problems easily accessible and encourage collaboration between avid mathematicians. It was created and is being developed by [our team](https://github.com/discord-contest-bot/). Check out the [Contest Bot Website!](https://cryptic-hamlet-37911.herokuapp.com/).')
     	.setThumbnail('https://images.topperlearning.com/mimg/topper/news/c1adbbdba02e149861919befc5cb6558.png?v=0.0.3')
     	.addFields(
-    		{ name: 'Help Command', value: 'You can use `[prefix] help` to get my attention.' },
-        { name: 'Prefix', value: 'The current prefixes are \`' + prefix + '\`, but a mention works perfectly fine (<@746943730510200893>).'},
-        { name: 'Contests', value: 'I have many contests available. Use `[prefix] contests` to see them all.'},
-        { name: 'Report a Bug', value: 'Use \`' + prefix + 'bug report [content]\` to report a bug!'},
-        { name: 'Suggest a Contest', value: 'Use \`' + prefix + 'suggest contest\` to start the contest suggestion helper!'},
-        { name: 'Support server', value: '[Join Us Here](https://discord.gg/C2sYVGb)'},
-        { name: 'Invite Me', value: '[Invite Contest Bot to Your Server](https://discord.com/api/oauth2/authorize?client_id=746943730510200893&permissions=100416&scope=bot)'},
+        { name: 'Prefix', value: 'The current prefix is \`' + prefix + '\`, but a mention works perfectly fine (<@746943730510200893>).'},
+        { name: 'What can Contest Bot do?', value: 'Contest Bot is truly one of the best resources a math competitor can have in their discord server. It can:\n* Give you images rendered from LaTeX from almost any math competition you can think of,\n* Provide you with the AoPS thread for the problem, and even\n* Provide the raw LaTeX code for the problem for your own requirements.\n\nIf a contest is not supported by Contest Bot, you can use the `suggest contest` command, or simply join our [support server](https://discord.gg/C2sYVGb) and we will add it for you **_instantly!_**'},
+        { name: 'Problems', value: 'In order to see how I can give you problems, use `' + prefix + ' problems`!'},
+        { name: 'Other Commands', value: 'In order to see other commands, use `' + prefix + ' other`!'},
+        { name: 'Support server', value: '[Join Us Here](https://discord.gg/C2sYVGb)!'},
+        { name: 'Invite Me', value: '[Invite Contest Bot to Your Server](https://discord.com/api/oauth2/authorize?client_id=746943730510200893&permissions=100416&scope=bot)!'},
     	)
     	.setTimestamp()
-    	.setFooter('Contact me at Circumrectangular Hyperbola#8766', 'https://i1.sndcdn.com/artworks-000219620854-jeksn1-t500x500.jpg');
+    	.setFooter('© Discord Contest Bot Team');
      message.channel.send(helpEmbed);
      return;
   }
+  if (message.content.toLowerCase() === 'problems') {
+    message.channel.send(`The central aspect of this bot is getting problems. You can do this with the command \`` + prefix + `\` followed by the competition name or alias, year, and problem number. If there are additional arguments (for example, HMMT subject tests and ISL subject categories) then they go in between the year and problem number. For example, if I wanted the 5th problem of the Harvard-MIT Math Tournament Algebra subject test, I could write \`` + prefix + ` HMMT Algebra 5.\`
+
+      Some finer points:
+
+      * To randomize the problem number, year, or subject, do not specify (currently a work in progress).
+      * For abbreviations of subject categories, if using one letter doesn't work, use two letters to see if it works.
+
+      If you do this properly, Contest Bot will send an image containing the problem. In addition, two reactions will appear on the message: 💻 and 🔗.
+      * Clicking on the 💻 will reveal the LaTeX code for the problem, if you want to use it on your own TeX document.
+      * Clicking on the 🔗 will reveal the url to the AoPS thread about the problem, if you would like to write your own solution that you're proud of or if you get stuck and want to see how the problem is solved.
+
+      Note that the bot automatically removes the reactions after one minute.`);
+    return;
+  }
+  if (message.content.toLowerCase() === 'other') {
+    message.channel.send(`In order to use a command, you can use \`` + prefix + `[command] [arguments]\` or substitute a ping for the prefix if you forget it.
+      Here are some of Contest Bot's other commands:
+      * \`contests\`
+      You can use this to see the full list of all available contests. React with the left or right arrows to browse the other pages (yes, there are that many contests!) In the list, you will see the alias of all competitions. Any of the keywords on the same line as the contest name can be used as aliases to request problems.
+      * \`suggest contest\`
+      This command is used if you want to suggest a contest that isn't already supported by Contest Bot. Just follow the prompts given by the bot, and soon you will be able to request problems from that contest!
+      * \`bug report [content]\`
+      If there is an issue with Contest Bot, use this command to tell us. Please be detailed, so that we know exactly what's bothering you.`);
+    return;
+  }
+
   if (message.content.toLowerCase().includes('creator')) {
     message.channel.send('Hi there', {files: ['creator-pfp.jpg']});
   }
